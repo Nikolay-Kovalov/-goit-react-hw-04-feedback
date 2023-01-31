5b89a78639a3c4a037835a7077d62d78f24aff73
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2023-01-31 13:14:31 +0200

    Edit files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,64 @@
+import { useState } from "react";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Section } from "./Section/Section";
+import { Notification } from "./Notification/Notification";
+import { Layout } from "./Layout";
+
 export const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
+
+    const addFeedback = feedback => {
+    switch (feedback) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        return;
+    }
+  };
+  
+  const countTotalFeedback = () => {
+    return good + neutral + bad
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) || 0
+  };
+ 
+
   return (
-    <>
-  Привет
-    </>
-  );
-};
+    <Layout>
+      <Section
+        title="Please leave feedback" />
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={addFeedback}
+          
+      />
+      <Section />
+      <Section title="Statistics" />
+      {(good > 0 || neutral > 0 || bad) ? <div>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+        <Section /></div> : <Notification message="There is no feedback" />}
+      
+    </Layout>
+  )
+}
+
